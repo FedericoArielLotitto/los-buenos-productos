1747f94cdbc06681a6e4235f1836f0807de5fe4d
Federico Ariel Lotitto <federicoariellotitto@gmail.com>
2022-01-22 20:27:29 -0300

    Cambia tipo de número de serie a un varchar(20)

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE producto(
     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre VARCHAR(45) NOT NULL,
-    numero_de_serie DECIMAL(20,0) NOT NULL,
+    numero_de_serie VARCHAR(20) NOT NULL,
     marca VARCHAR(45),
     cantidad BIGINT
 );
